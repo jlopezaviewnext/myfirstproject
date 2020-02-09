8f407ed87bbbc58a86ae917c779dd023c6d63793
JAVIER LOPEZ AZNAR <0012752@insags.com>
2020-02-09 19:53:58 +0100

    Text v2

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 var express = require('express');
 var app = express();
 app.get('/', function (req, res) {
-res.send('Hello, World! aquí ando. No le gusta a Dani esto..... \n');
+res.send('Hello, World! aquí ando. Está es la versión 2..... \n');
 });
 app.listen(8080, function () {
-console.log('Example app listening on port 8080!');
+console.log('Example app listening on port 8080! v2');
 });
 module.exports = app;
